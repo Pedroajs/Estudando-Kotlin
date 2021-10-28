6d8c9bfa89ce2e58831192b3e95e07571c874035
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-10-28 17:59:58 -0200

    inicio de estudos de Kotlin

---
+++ src/fundamentos/Comentarios.kt
@@ -0,0 +1,18 @@
+package fundamentos
+
+
+/**
+ O kotlin suporta o KDoc
+ @param lista de parametros passados por linha de comando (notations)
+
+ */
+fun main(args: Array<String>)
+{
+    //comentario de uma linha
+    println("Vendo comentários")
+
+    /*
+        comentario de mais de uma linha
+        é diferente do KDoc
+    */
+}
---
+++ src/fundamentos/Variaveis.kt
@@ -0,0 +1,19 @@
+package fundamentos
+
+fun main(args: Array<String>)
+{
+    // aqui declaramos a variável e atribuímos o tipo Int(numero inteiro) a ela.
+    var a: Int
+
+    /*
+        Já nesse caso, a partir do tipo de dado atribuído à variável, o compilador
+        infere e aribui o tipo à variável
+    **/
+    var b = 2
+
+    // tda variavel em kotlin tem um tipo fixo
+
+    a = 10
+
+    print(a+b)
+}
