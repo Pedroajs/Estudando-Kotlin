555ccef65bb5351d665bd8de34927c84ebf9ebd1
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-10-29 01:26:20 -0200

    usando dot noation

---
+++ src/fundamentos/NotacaoPonto.kt
@@ -0,0 +1,9 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    val a: Int = 33.dec()
+    val b: String = a.toString()
+
+    println("Int: $a")
+    println("Primeiro char da string b é: ${b.first()}")
+}
