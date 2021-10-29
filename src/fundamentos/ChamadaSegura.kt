d22ae8578ae366910f66764be54a68a226c358df
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-10-29 13:35:45 -0200

    safe call operator

---
+++ src/fundamentos/ChamadaSegura.kt
@@ -0,0 +1,12 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    /*
+     o operador ? é oq se chama de safe call operator
+     indica que essa variavel é opcional e pode ter um valor
+     nulo/ nao ter valor
+    */
+
+    var a: Int? = null
+}
+
