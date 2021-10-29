0ab1ae268835247b2a498d1fa09458049f3fd676
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-10-29 13:49:25 -0200

    elvis operator

---
+++ src/fundamentos/OperadorElvis.kt
@@ -0,0 +1,14 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    val opcional:String? = null
+    val obrigatorio: String = opcional ?: "valor padrão"
+    println(obrigatorio)
+
+    /*
+    * com o elvis operator é possivel atribuir um valor padrao a uma variavel que não se
+    * tenha certeza se tem valor. Evitando erros
+    *
+    * 
+    * */
+}
