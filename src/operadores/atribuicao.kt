361b6f340a1be5066b9eae66cf50332b2c52b199
pedroajs <pedroajs93@gmail.com>
2021-11-08 15:22:52 -0300

    operador de atribuicao

---
+++ src/operadores/atribuicao.kt
@@ -0,0 +1,14 @@
+package operadores
+
+fun main() {
+    var a: Int = 7
+    var b: Int = 3
+
+    b +=a // atribuicao aditiva
+    b -= 4 // atribuicao subtrativa
+    b *= 2 // atribuicao multiplicativa
+    b /=2 // atribuicao divisiva
+    b %=2 // atribuicao de modulo
+
+    print(b)
+}
