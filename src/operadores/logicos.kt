c0ace368b3b713471eeaa6743d9599669499b365
pedroajs <pedroajs93@gmail.com>
2021-11-08 23:13:01 -0300

    operadores logicos e xor

--- src/operadores/logicos.kt
+++ src/operadores/logicos.kt
@@ -1,4 +1,17 @@
 package operadores
 
-class logicos {
+fun main(args: Array<String>) {
+    val executouTrabalho1: Boolean = false
+    val executouTrabalho2: Boolean = true
+
+    val comprouSorvete: Boolean = executouTrabalho1 || executouTrabalho2
+    val comprouTv50: Boolean = executouTrabalho1 && executouTrabalho2
+    val comprouTv32: Boolean = executouTrabalho1 xor executouTrabalho2 // funcao infix para ou exclusivo
+
+    println(comprouSorvete)
+    println(comprouTv50)
+    println(comprouTv32)
+
+    //operador unário
+    if(!comprouSorvete) println("A saúde agradece")
 }
