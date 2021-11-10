794d17770d3418f41ea017edd35b38561dc19114
pedroajs <pedroajs93@gmail.com>
2021-11-09 23:49:20 -0300

    Vendo o Smartcast do kotlin

---
+++ src/fundamentos/smartCast.kt
@@ -0,0 +1,22 @@
+package fundamentos
+
+fun souEsperto(x: Any){
+    if(x is String){
+        println(x.toUpperCase())
+    } else if(x is Int){
+        println(x.plus(3))
+    }
+}
+
+fun souEsperto2(x: Any){
+    when(x){
+        is String -> println(x.toUpperCase())
+        is Int -> println(x.plus(3))
+        else -> println("Repense a sua vida")
+    }
+}
+
+fun main(args: Array<String>) {
+    souEsperto(2)
+    souEsperto2("banana")
+}
