05ff53fd3f7692d02725729ce990faa28375ab8d
pedroajs <pedroajs93@gmail.com>
2021-11-10 22:34:37 -0300

    paramentros sao imutaveis

---
+++ src/funcoes/ParametrosImutavies.kt
@@ -0,0 +1,11 @@
+package funcoes
+
+fun incremento(num:Int){
+    num++
+    // essa é uma operacao proibida, pois éuma reatribuição.
+    // parametros de funcao são constantes
+}
+
+fun main() {
+
+}
